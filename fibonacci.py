bea8db76563ce4de5cc9d9cbc34635641a15da20
Alexander Khubeis-Alves <akhubeis@uoguelph.ca>
2025-10-03 13:04:14 -0400

    Implement user input for Fibonacci sequence
    Added user input validation and Fibonacci sequence printing.

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,26 @@
-#!/usr/bin/env python3
+print('User input: ', end='')
+amount = input()
 
-# Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
+while not amount.isdigit() or int(amount) < 0:
+    print('Expected output: Please enter a positive integer.')
+    print()
+    print('User input: ', end='')
+    amount = input()
+
+amount = int(amount)
+print('Expected output: ', end='')
+num_one = 0
+num_two = 1
+for i in range(amount):
+    print(num_one, end='')
+    if i < amount - 1:
+        print(' ', end='')
+    temp = num_one
+    num_one = num_two
+   num_two = temp + num_two
+
+print() 
+
+print("have a nice day :)")
+
+#pasted from command prompt
